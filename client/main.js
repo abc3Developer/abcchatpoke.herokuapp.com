1cfeed4926191188149a26fd6b778eeb758e6bf5
abc3developer <adrian260798asdfg@gmail.com>
2020-08-23 19:15:40 +0200

    abc18

--- client/main.js
+++ client/main.js
@@ -1,27 +1,88 @@
 var socket = io();
+  var container = document.querySelector('#section');
 
-var form = document.querySelector('form');
+var newMessage1 = document.querySelector('#newMessage1');
+var newMessage2 = document.querySelector('#newMessage2');
 
-form.addEventListener('submit', function(e) {
+var form = document.querySelector('#form');
+var  posit = 0;
+var abcc4 = 0;
+var player = document.querySelector('#player');
+var playeronline = document.querySelector('#playeronline');
+var input = document.querySelector('#message');
+var text = 0;
+
+
+
+function abconline() {
+  player = document.querySelector('#player');
+   playeronline = document.querySelector('#playeronline');
+   form = document.querySelector('#form');
+  setInterval(() => {
+ posit = player.getAttribute('position');
+ abcc4 = 1;
+ input.value = posit;
+ form.checked = true;
+ }, 50);
+ }
+
+form.addEventListener('change', function(e) {
   e.preventDefault();
   colorbtn = document.querySelector('#colorbtn');
-  var input = document.querySelector('#message');
-  var text = colorbtn.style.backgroundColor+" :"+input.value;
+   input = document.querySelector('#message');
+   text = input.value;
   socket.emit('message', text);
   input.value = '';
+
 });
 
 socket.on('message', function(text) {
   if (!text) {
     return;
   }
-  var container = document.querySelector('section');
-  var newMessage = document.createElement('p');
-  newMessage.innerText = text;
-  container.appendChild(newMessage);
+  newMessage1 = document.querySelector('#newMessage1');
+newMessage2 = document.querySelector('#newMessage2');
+form = document.querySelector('#form');
 
-  var seperator = document.createElement('br');
-  container.appendChild(seperator);
+  if (colorbtn.style.backgroundColor == 'red') {
+   newMessage1.innerText = text;
+  }
+  if (colorbtn.style.backgroundColor == 'blue') {
+     newMessage2.innerText = text;
+  }
 
+form.checked = false;
+  if (abcc4 == 1) {
+    abconline2();
+  }
   container.scrollTop = container.scrollHeight;
 });
+
+function abconline2() {
+  playeronline = document.querySelector('#playeronline');
+  let positionnavity4 = document.createAttribute('position');
+  if (colorbtn.style.backgroundColor == 'red') {
+ positionnavity4.value = newMessage1.innerText;
+  }
+  if (colorbtn.style.backgroundColor == 'blue') {
+positionnavity4.value = newMessage2.innerText;
+  }
+
+  playeronline.setAttributeNode(positionnavity4);
+}
+
+
+
+
+
+function iniciaronline() {
+  if (colorbtn.style.backgroundColor == 'red') {
+    for (let index = 0; index < p.length; index++) {
+      const element = p[index];
+      
+    }
+  }
+  if (colorbtn.style.backgroundColor == 'red') {
+    
+  }
+}
