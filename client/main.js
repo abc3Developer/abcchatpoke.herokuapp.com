c9b4198e000278ad47c9d2f3d073d7eee60e7a69
abc3developer <adrian260798asdfg@gmail.com>
2020-08-24 14:01:41 +0200

    abc101

--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
 var player = document.querySelector('#player');
-var player1 = document.querySelector('#player1');
-var player2 = document.querySelector('#player2');
   var playeronline1 = document.querySelector('#playeronline1');
    var playeronline2 = document.querySelector('#playeronline2');
 
@@ -13,7 +11,11 @@ var player2 = document.querySelector('#player2');
    var abc = 0;
    var btncolor = document.querySelector('#btncolor');
    var btnck = document.querySelector('#btnck');
-   
+   var input = document.querySelector('#message');
+   var text = 0;
+
+   var posit = 0;
+   var posit2 = 0;
    function ab12() {
      btncolor = document.querySelector('#btncolor');
      abc = 'abc12';
@@ -33,11 +35,15 @@ var player2 = document.querySelector('#player2');
      if (btnck.checked == true) {
        starrt();
      }
+     input = document.querySelector('#message');
+     posit = player.getAttribute('position');
+     posit2 = posit["x"]+ " " +posit["y"]+ " " +posit["z"];
+     input.value = posit2;
    }
    
    function starrt() {
-     var input = document.querySelector('#message');
-     var text = input.value;
+     input = document.querySelector('#message');
+     text = input.value;
      if (abc == 'abc12') {
        console.log(12)
        abc = 'a1b2c';
